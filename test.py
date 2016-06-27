d3a70712ee364ef3f02e8b559e3514279c356cd5
annatorok <mail@annatorok.com>
2016-06-27 10:09:50 +0200

    Add test to third exercise

---
+++ test.py
@@ -0,0 +1,36 @@
+import unittest
+import third
+
+class Test_count_letter_in_string(unittest.TestCase):
+
+    def test_count_if_empty_letter_empty_string(self):
+        self.assertEqual(third.count_letter_in_string('', ''), 0)
+
+    def test_count_if_empty_string(self):
+        self.assertEqual(third.count_letter_in_string('', 'b'), 0)
+
+    def test_count_with_int(self):
+        self.assertEqual(third.count_letter_in_string(3483, '3'), 0)
+
+    def test_count_with_float(self):
+        self.assertEqual(third.count_letter_in_string(129.5, '2'), 0)
+
+    def test_count_if_string(self):
+        self.assertEqual(third.count_letter_in_string('almafa', 'a'), 3)
+
+    def test_count_if_list(self):
+        self.assertEqual(third.count_letter_in_string([1,4,8,2,8], '8'), 0)
+
+    def test_count_if_true_or_not(self):
+        self.assertEqual(third.count_letter_in_string(True, 'u'), 0)
+
+    def test_count_with_hypen_letters(self):
+        self.assertEqual(third.count_letter_in_string('lomha-macska', 'a'), 3)
+
+    def test_count_with_uppercase_letters(self):
+        self.assertNotEqual(third.count_letter_in_string('asztalavisZTA', 'a'), 4)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
