7d0fb45c5070c46f3cc9ff3e942ad6af86940b50
annatorok <mail@annatorok.com>
2016-06-27 09:45:55 +0200

    Add solution to second exercise

--- second.py
+++ second.py
@@ -4,3 +4,13 @@
 # it should write "appleappleapple" to the file "tree.txt".
 # the function should not raise an error on any output problem, for example
 # denied permission
+
+def string_three_times_to_text(filename, string):
+    f = open(filename, 'w')
+    try:
+        f.write(string * 3)
+        f.close()
+    except:
+        pass
+
+print(string_three_times_to_text('text.txt', 'exam'))
