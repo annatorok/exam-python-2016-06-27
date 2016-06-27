84b7f63199a4f72e244ad3f6eb9fd51d4675dd4f
annatorok <mail@annatorok.com>
2016-06-27 09:30:37 +0200

    Solution for first exercise

--- first.py
+++ first.py
@@ -3,3 +3,11 @@
 # It should raise an error if the parameter is not a list
 # example: [1, 2, 3, 4, 5] should produce [2, 4]
 
+def every_second_element(input):
+    if type(input) != list:
+        raise TypeError('Expected a list')
+    new_list = []
+    new_list += input[1::2]
+    return new_list
+
+print(every_second_element([1,2,3,4,5]))
